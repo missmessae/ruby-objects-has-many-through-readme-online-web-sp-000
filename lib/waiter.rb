27269809c4547cf9a2000f79e08a8ae7930794a6
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 22:46:15 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,16 +1,16 @@
 class Waiter
+  attr_accessor :name, :yrs_experience
 
-    attr_accessor :name, :yrs_experience
+  @@all = []
 
-    @@all = []
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
 
-    def initialize(name, yrs_experience)
-      @name = name
-      @yrs_experience = yrs_experience
-      @@all << self
-    end
-
-    def self.all
-      @@all
-    end
+  def self.all
+    @@all
+  end
+  
 end
